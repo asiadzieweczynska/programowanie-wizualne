ca7161000d43b02ff82b922d492b17210a0df9ee
Joanna Dzieweczyńska <joanna.dzieweczynska@gmail.com>
2026-03-05 23:03:12 +0100

    Projekt 1 zadanie 2.3

---
+++ Calculation/calculate.cs
@@ -0,0 +1,29 @@
+﻿namespace Calculation
+{
+    public class calculate
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Nie mozna dzielic przez zero.");
+            }
+            return a / b;
+        }
+    }
+}
