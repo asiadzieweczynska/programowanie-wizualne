97fbf9c7ebde42f27368e7244fb0c5591ac8d26a
Joanna Dzieweczyńska <joanna.dzieweczynska@gmail.com>
2026-03-05 23:12:42 +0100

    projekt 1 zadanie 2.4

---
+++ Projekt1/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Projekt1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(82, 231);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 0;
+            button1.Text = "dodawanie";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(216, 231);
+            button2.Name = "button2";
+            button2.Size = new Size(154, 34);
+            button2.TabIndex = 1;
+            button2.Text = "odejmowanie";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(397, 231);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 2;
+            button3.Text = "mnożenie";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(539, 231);
+            button4.Name = "button4";
+            button4.Size = new Size(112, 34);
+            button4.TabIndex = 3;
+            button4.Text = "dzielenie";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(321, 105);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(269, 31);
+            textBox1.TabIndex = 4;
+            textBox1.Text = "1";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(321, 159);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(269, 31);
+            textBox2.TabIndex = 5;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(321, 298);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(269, 31);
+            textBox3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(160, 108);
+            label1.Name = "label1";
+            label1.Size = new Size(132, 25);
+            label1.TabIndex = 7;
+            label1.Text = "Pierwsza liczba:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(160, 162);
+            label2.Name = "label2";
+            label2.Size = new Size(114, 25);
+            label2.TabIndex = 8;
+            label2.Text = "Druga liczba:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(160, 301);
+            label3.Name = "label3";
+            label3.Size = new Size(65, 25);
+            label3.TabIndex = 9;
+            label3.Text = "Wynik:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ Projekt1/Form1.cs
@@ -0,0 +1,68 @@
+using Calculation;
+
+namespace Projekt1
+{
+    public partial class Form1 : Form
+    {
+        private readonly calculate calc = new calculate();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool GetNumbers(out int a, out int b)
+        {
+            a = 0;
+            b = 0;
+            if (!int.TryParse(textBox1.Text, out a))
+            {
+                MessageBox.Show("Podaj poprawną pierwszą liczbę.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            if (!int.TryParse(textBox2.Text, out b))
+            {
+                MessageBox.Show("Podaj poprawną drugą liczbę.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (GetNumbers(out int a, out int b))
+                textBox3.Text = calc.Add(a, b).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (GetNumbers(out int a, out int b))
+                textBox3.Text = calc.Subtract(a, b).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (GetNumbers(out int a, out int b))
+                textBox3.Text = calc.Multiply(a, b).ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (GetNumbers(out int a, out int b))
+            {
+                try
+                {
+                    textBox3.Text = calc.Divide(a, b).ToString();
+                }
+                catch (DivideByZeroException)
+                {
+                    MessageBox.Show("Nie można dzielić przez zero.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
