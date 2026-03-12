df15446397660953052a9c60058ef60cdeb68ecb
Joanna Dzieweczyńska <joanna.dzieweczynska@gmail.com>
2026-03-12 07:06:29 +0100

    usunięcie projektu 1

--- HelloWorld/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Wersja 2");
-Console.WriteLine("bla bla bla bla");
-Console.WriteLine("tralalala");
