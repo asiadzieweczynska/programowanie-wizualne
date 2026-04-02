1629b447433960ffe6c0eecb57cd2404488a47c2
Joanna Dzieweczyńska <joanna.dzieweczynska@gmail.com>
2026-04-02 14:09:00 +0200

    Rotate commit

--- projekt4/Form1.Designer.cs
+++ projekt4/Form1.Designer.cs
@@ -28,12 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pictureBox2 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox2.Location = new Point(201, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(352, 426);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(45, 173);
+            button1.Name = "button1";
+            button1.Size = new Size(129, 29);
+            button1.TabIndex = 2;
+            button1.Text = "rotate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(45, 230);
+            button2.Name = "button2";
+            button2.Size = new Size(129, 29);
+            button2.TabIndex = 3;
+            button2.Text = "invertColors";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(45, 295);
+            button3.Name = "button3";
+            button3.Size = new Size(129, 29);
+            button3.TabIndex = 4;
+            button3.Text = "upsideDown";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(45, 355);
+            button4.Name = "button4";
+            button4.Size = new Size(129, 29);
+            button4.TabIndex = 5;
+            button4.Text = "OnlyGreen";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(45, 409);
+            button5.Name = "button5";
+            button5.Size = new Size(129, 29);
+            button5.TabIndex = 6;
+            button5.Text = "Load";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Checked = true;
+            radioButton1.Location = new Point(45, 23);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(50, 24);
+            radioButton1.TabIndex = 7;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "90 ";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(45, 68);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(54, 24);
+            radioButton2.TabIndex = 8;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "180";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(45, 117);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(54, 24);
+            radioButton3.TabIndex = 9;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "270";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(572, 450);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox2);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
     }
 }
--- projekt4/Form1.cs
+++ projekt4/Form1.cs
@@ -5,6 +5,75 @@ namespace projekt4
         public Form1()
         {
             InitializeComponent();
+
+            // Domyślnie zawsze 90°.
+            radioButton1.Checked = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image is null)
+            {
+                MessageBox.Show(this, "Najpierw wczytaj obraz (Load).", "Brak obrazu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            var rotateFlipType = RotateFlipType.Rotate90FlipNone;
+            if (radioButton2.Checked)
+            {
+                rotateFlipType = RotateFlipType.Rotate180FlipNone;
+            }
+            else if (radioButton3.Checked)
+            {
+                rotateFlipType = RotateFlipType.Rotate270FlipNone;
+            }
+
+            try
+            {
+                using var source = new Bitmap(pictureBox2.Image);
+                source.RotateFlip(rotateFlipType);
+
+                var rotated = new Bitmap(source);
+                var oldImage = pictureBox2.Image;
+                pictureBox2.Image = rotated;
+                oldImage?.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, $"Nie udało się obrócić obrazu.\n\n{ex.Message}", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using var dialog = new OpenFileDialog
+            {
+                Title = "Wybierz zdjęcie",
+                Filter = "Pliki obrazów|*.png;*.jpg;*.jpeg;*.bmp;*.gif;*.tif;*.tiff|Wszystkie pliki|*.*",
+                RestoreDirectory = true,
+                CheckFileExists = true,
+                CheckPathExists = true,
+            };
+
+            if (dialog.ShowDialog(this) != DialogResult.OK)
+            {
+                return;
+            }
+
+            try
+            {
+                using var stream = new FileStream(dialog.FileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+                using var image = Image.FromStream(stream);
+                var newImage = new Bitmap(image);
+
+                var oldImage = pictureBox2.Image;
+                pictureBox2.Image = newImage;
+                oldImage?.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, $"Nie udało się wczytać obrazu.\n\n{ex.Message}", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
