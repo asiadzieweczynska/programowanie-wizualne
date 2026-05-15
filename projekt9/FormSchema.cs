b7e6e84a8b0a2b49e60b83f23bc038148f701b6f
Joanna Dzieweczyńska <joanna.dzieweczynska@gmail.com>
2026-05-15 15:28:56 +0200

    projekt9

---
+++ projekt9/FormSchema.cs
@@ -0,0 +1,23 @@
+namespace projekt9;
+
+public static class FormSchema
+{
+    public static readonly string[] Labels =
+    [
+        "Poznań, dnia (wnioskodawca)",
+        "Numer albumu",
+        "Nazwisko i imię",
+        "Semestr, rok",
+        "Kierunek i stopień studiów",
+        "Przedmiot",
+        "Punkty",
+        "Prowadzący",
+        "Uzasadnienie wniosku",
+        "Data i podpis studenta",
+        "Decyzja (wyrażam/nie wyrażam zgody)",
+        "Skład komisji (1)",
+        "Skład komisji (2)",
+        "Skład komisji (3)",
+        "Poznań, dnia / Pieczątka i podpis",
+    ];
+}
